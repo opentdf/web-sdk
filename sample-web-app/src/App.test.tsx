09603449f4f9530fdd194a24000e35767f6a4be9
Dave Mihalcik <dmihalcik@virtru.com>
2021-10-28 08:58:45 -0400

    ✨ Initial test of file loading in web app
    Demonstrate reading file contents in SPA using code defined in the client lib.
    
    Also build fixes:
    
    - simplify makefile a bit, and fail fast on ci failure
    - fix lib not updating in child deps, i hope
    - update everything to tsc 4.5 beta, required for exports block
    
    huh why is spa flaky
    
    - it looks like sometimes tsc decides to say:
    
    > Error: [14:35:27] [@snowpack/plugin-typescript] src/App.tsx(5,33): error TS1471: Module '@opentdf/client/chunkers.js' cannot be imported using this construct. The specifier only resolves to an ES module, which cannot be imported synchronously. Use dynamic import instead.
    
    Is this true? I'd rather have snowpack load everything into one file so I don't want this, right?
    
    Also, why does it work locally (sometimes?)
    
    Update package.json
    
    Update package.json
    
    Update web-test-runner.config.cjs
    
    move check version into ci workflow file
    
    make format
    
    Update package.json
    
    Create chunkers.spec.ts
    
    Update package.json
    
    ugh mauybe
    
    roll back changes to tsc version
    
    ugh doesn't fix it
    
    trying
    
    npm i
    
    Update Makefile
    
    Update .nvmrc
    
    consolidating lint into CI
    
    simplify app a bit

--- sample-web-app/src/App.test.tsx
+++ sample-web-app/src/App.test.tsx
@@ -1,12 +1,37 @@
 import * as React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { expect } from 'chai';
-import App from './App';
+import App from './App.jsx';
 
 describe('<App>', () => {
-  it('renders learn react link', () => {
-    const { getByText } = render(<App />);
-    const linkElement = getByText(/sum\(1\,2\)\s*=\s*3/i);
-    expect(document.body.contains(linkElement));
+  it('renders', () => {
+    render(<App />);
+    const element = screen.getByText(/sum\(1\,2\)\s*=\s*3/i);
+    expect(document.body.contains(element));
+  });
+
+  it('loads files', async () => {
+    const file = new File(['hello, world!'], 'hello.txt', { type: 'text/plain' });
+    render(<App />);
+    const input = screen.getByLabelText(/select file/i) as HTMLInputElement;
+    userEvent.upload(input, file);
+    const filenameEl = await screen.findByText(/hello.txt/i);
+    expect(document.body.contains(filenameEl));
+  });
+
+  it('processes files', async () => {
+    console.log("process a");
+    const file = new File(['hello, world!'], 'hello.txt', { type: 'text/plain' });
+    render(<App />);
+    const input = screen.getByLabelText(/select file/i) as HTMLInputElement;
+    userEvent.upload(input, file);
+    await screen.findByText(/hello.txt/i);
+    const processButton = screen.getByText(/process/i) as HTMLInputElement;
+    console.log("handles f", processButton);
+    userEvent.click(processButton);
+    console.log("handles g");
+    // const el = await screen.findByText(/start[:] 68/i);
+    // console.log(el);
   });
 });
