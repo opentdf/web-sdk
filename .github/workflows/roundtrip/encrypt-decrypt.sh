5ef1d2317b3ef01ae9e83d8dda5779efd1be054d
Elizabeth Healy <ehealy@virtru.com>
2022-04-26 17:42:03 -0400

    cleanup

--- .github/workflows/roundtrip/encrypt-decrypt.sh
+++ .github/workflows/roundtrip/encrypt-decrypt.sh
@@ -14,7 +14,7 @@ npx @opentdf/cli --log-level DEBUG \
 --kasEndpoint http://localhost:65432/api/kas \
 --oidcEndpoint http://localhost:65432 \
 --auth tdf:tdf-client:123-456 \
---output sample.txt.tdf encrypt sample.txt
+--output sample.txt.tdf encrypt sample.txt \
 --attributes https://example.com/attr/Classification/value/S,https://example.com/attr/COI/value/PRX
 
 npx @opentdf/cli --log-level DEBUG \
