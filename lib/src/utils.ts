955d6c0cd7dc540099d20126d406da6500f712ff
ivanovSPvirtru <41641307+ivanovSPvirtru@users.noreply.github.com>
2022-07-05 13:20:32 +0300

    fixes for safari and firefox (#60)
    * fixes for safari and firefox
    
    * subclass need workaround too
    
    * check for browser added
    
    * utils fixed

--- lib/src/utils.ts
+++ lib/src/utils.ts
@@ -1,3 +1,11 @@
 export function isBrowser() {
   return typeof window !== 'undefined'; // eslint-disable-line
 }
+
+export const isSafari = (): boolean =>
+  /constructor/i.test(String(globalThis.HTMLElement)) ||
+  // @ts-ignore
+  globalThis?.safari?.pushNotification?.toString() === '[object SafariRemoteNotification]';
+
+// @ts-ignore
+export const isFirefox = (): boolean => typeof InstallTrigger !== 'undefined';
