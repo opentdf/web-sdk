5b5adb7e7f1456f7ba2c24af606b19876e37f509
sivanov <sivanov.ctr@virtru.com>
2022-07-05 12:53:37 +0300

    check for browser added

--- lib/src/utils.ts
+++ lib/src/utils.ts
@@ -1,3 +1,11 @@
 export function isBrowser() {
   return typeof window !== 'undefined'; // eslint-disable-line
 }
+
+export const isSafari(): Boolean => (
+  /constructor/i.test(globalThis.HTMLElement) ||
+  globalThis?.safari?.pushNotification?.toString() === '[object SafariRemoteNotification]'
+);
+
+export const isFirefox(): Boolean => typeof InstallTrigger !== 'undefined';
+
