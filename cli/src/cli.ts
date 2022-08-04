b3155bb8d3268d17ab15a420d7b0d0c50f1b8f9d
Andrew Krokhmaliuk <andrii.krokhmaliuk@virtru.com>
2022-08-04 17:14:13 +0300

    Review fixes & Fix pipeline

--- cli/src/cli.ts
+++ cli/src/cli.ts
@@ -1,7 +1,7 @@
 import yargs from 'yargs';
 import { readFile, stat, writeFile } from 'fs/promises';
 import { hideBin } from 'yargs/helpers';
-import { NanoTDFClient, AuthProviders, version } from '@opentdf/client/nano-node-esm';
+import { NanoTDFClient, AuthProviders, version } from '@opentdf/client';
 
 import { CLIError, Level, log } from './logger.js';
 
