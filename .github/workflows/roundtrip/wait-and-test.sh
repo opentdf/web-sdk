e7c631139a4644080b018a09e52c20571083af80
David Mihalcik <dmihalcik@virtru.com>
2022-08-30 15:22:28 -0400

    🔨 CI workflow improvements
    - Move format out to its own workflow since it is all by itself anyway and kinda different
    - improve error messaging
    - Don't re-install npm in the retry loop; only do that once
    -

--- .github/workflows/roundtrip/wait-and-test.sh
+++ .github/workflows/roundtrip/wait-and-test.sh
@@ -1,7 +1,23 @@
 #!/usr/bin/env bash
 
 APP_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
+ROOT_DIR="$(cd "${APP_DIR}/../../.." >/dev/null && pwd)"
+
 APP="${APP_DIR}/encrypt-decrypt.sh"
+
+_configure_app() {
+  cli_version=$(cd "${ROOT_DIR}/cli" && node -p "require('./package.json').version")
+  if [ -f "${ROOT_DIR}/opentdf-cli-${cli_version}.tgz" ]; then
+    echo "installing tgz"
+    cp "${ROOT_DIR}/opentdf-cli-${cli_version}.tgz" "${ROOT_DIR}/cli/"
+    (
+      cd "${APP_DIR}" || exit 1
+      npm uninstall @opentdf/cli && npm ci && npm i "../../../cli/opentdf-cli-${cli_version}.tgz"
+    )
+  else
+    npm i
+  fi
+}
 
 _wait-for() {
   echo "[INFO] In retry loop for quickstarted opentdf backend..."
@@ -10,7 +26,8 @@ _wait-for() {
     if sh "${APP}"; then
       return 0
     fi
-    if [[ $i == $limit ]]; then
+    if [[ $i == "$limit" ]]; then
+      echo "[WARN] Breaking _wait-for loop as we are at limit"
       break
     fi
     sleep_for=$((10 + i * i * 2))
@@ -21,4 +38,5 @@ _wait-for() {
   exit 1
 }
 
+_configure_app
 _wait-for
