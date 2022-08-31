08b836e7b17f06684cf8d3498c4fa665839633c7
Dave Mihalcik <38867245+dmihalcik-virtru@users.noreply.github.com>
2022-08-31 16:10:22 -0400

    🔨 CI workflow improvements (#83)
    - Move format out to its own workflow since it is all by itself anyway and kinda different
    - improve error messaging
    - Don't re-install npm in the retry loop; only do that once

--- .github/workflows/roundtrip/encrypt-decrypt.sh
+++ .github/workflows/roundtrip/encrypt-decrypt.sh
@@ -1,26 +1,20 @@
 #!/usr/bin/env bash
 set -e
 
-cd ../..
-pwd
-
-cp ../../../opentdf-cli-*.tgz ../../../cli
-npm uninstall @opentdf/cli && npm ci && npm i ../../../cli/opentdf-cli-*.tgz
-
-echo "Hello World!" > ./sample.txt
+echo "Hello World!" >./sample.txt
 
 npx @opentdf/cli --log-level DEBUG \
---kasEndpoint http://localhost:65432/api/kas \
---oidcEndpoint http://localhost:65432 \
---auth tdf:tdf-client:123-456 \
---output sample.txt.tdf encrypt sample.txt \
---attributes https://example.com/attr/Classification/value/S,https://example.com/attr/COI/value/PRX
+  --kasEndpoint http://localhost:65432/api/kas \
+  --oidcEndpoint http://localhost:65432/auth \
+  --auth tdf:tdf-client:123-456 \
+  --output sample.txt.tdf encrypt sample.txt \
+  --attributes https://example.com/attr/Classification/value/S,https://example.com/attr/COI/value/PRX
 
 npx @opentdf/cli --log-level DEBUG \
---kasEndpoint http://localhost:65432/api/kas \
---oidcEndpoint http://localhost:65432 \
---auth tdf:tdf-client:123-456 \
---output sample_out.txt decrypt sample.txt.tdf
+  --kasEndpoint http://localhost:65432/api/kas \
+  --oidcEndpoint http://localhost:65432 \
+  --auth tdf:tdf-client:123-456 \
+  --output sample_out.txt decrypt sample.txt.tdf
 
 diff sample.txt sample_out.txt
 
