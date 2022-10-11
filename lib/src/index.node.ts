839958f1031f871029ac77bde6dac293dc97daaf
David Mihalcik <dmihalcik@virtru.com>
2022-10-11 10:57:56 -0400

    ugh require node 18

--- lib/src/index.node.ts
+++ lib/src/index.node.ts
@@ -1,10 +1,8 @@
 import { NanoTDFClient, NanoTDFDatasetClient, AuthProviders, version, clientType } from './index';
 // @ts-ignore
-import fetch from 'node-fetch';
 import { webcrypto as crypto } from 'node:crypto';
 
 globalThis.crypto ??= crypto as unknown as Crypto;
-globalThis.fetch ??= fetch as typeof globalThis.fetch;
 
 export { NanoTDFClient, NanoTDFDatasetClient, AuthProviders, version, clientType };
 // for cjs webpack build default export needed
