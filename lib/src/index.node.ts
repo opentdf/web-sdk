0000582d2c94be3ede9e91dd3ad370630edfd3c4
David Mihalcik <dmihalcik@virtru.com>
2022-10-11 09:44:38 -0400

    Revert "Attempt 1: dynamic import"
    This reverts commit 805900c4f4b5810f2dd27fdc12ab35b50665e6a7.

--- lib/src/index.node.ts
+++ lib/src/index.node.ts
@@ -1,11 +1,9 @@
 import { NanoTDFClient, NanoTDFDatasetClient, AuthProviders, version, clientType } from './index';
-import { type RequestInfo, type RequestInit } from 'node-fetch';
+// @ts-ignore
+import fetch from 'node-fetch';
 import { webcrypto as crypto } from 'node:crypto';
 
 globalThis.crypto ??= crypto as unknown as Crypto;
-
-const fetch = (url: RequestInfo, init?: RequestInit) =>
-  import(/* webpackIgnore: true */ 'node-fetch').then(({ default: fetch }) => fetch(url, init));
 globalThis.fetch ??= fetch as typeof globalThis.fetch;
 
 export { NanoTDFClient, NanoTDFDatasetClient, AuthProviders, version, clientType };
