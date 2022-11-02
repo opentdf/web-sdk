7993d18aa5fc2ec9fafdf35b4499d0390f2254fe
David Mihalcik <dmihalcik@virtru.com>
2022-11-02 14:54:32 -0400

    Update encrypt-decrypt.sh

--- .github/workflows/roundtrip/encrypt-decrypt.sh
+++ .github/workflows/roundtrip/encrypt-decrypt.sh
@@ -8,14 +8,15 @@ cd "$APP_DIR"
 counter=0
 
 _nano_test() {
-  var=$((counter + 1))
-  echo "Hello World ${counter}" >./sample.txt
+  counter=$((counter + 1))
+  plain="./sample-${counter}.txt"
+  echo "Hello World ${counter}" >"./${plain}"
   npx "$1" --log-level DEBUG \
     --kasEndpoint http://localhost:65432/api/kas \
     --oidcEndpoint http://localhost:65432/auth/realms/tdf \
     --auth tdf-client:123-456 \
     --output sample.txt.ntdf \
-    encrypt sample.txt \
+    encrypt "${plain}" \
     --attributes https://example.com/attr/Classification/value/S,https://example.com/attr/COI/value/PRX
 
   [ -f sample.txt.ntdf ]
@@ -27,24 +28,25 @@ _nano_test() {
     --output sample_out.txt \
     decrypt sample.txt.ntdf
 
-  [ -f sample_out.txt ] && diff sample.txt sample_out.txt
+  [ -f sample_out.txt ] && diff "${plain}" sample_out.txt
 
   echo "Roundtrip nanotdf $1 -> $2 successful!"
-  rm -f sample.txt sample.txt.ntdf sample_out.txt
+  rm -f "${plain}" sample.txt.ntdf sample_out.txt
 }
 
 _nano_test @opentdf/cli-commonjs @opentdf/cli
 _nano_test @opentdf/cli @opentdf/cli-commonjs
 
 _tdf3_test() {
-  var=$((counter + 1))
-  echo "Hello World ${counter}" >./sample.txt
+  counter=$((counter + 1))
+  plain="./sample-${counter}.txt"
+  echo "Hello World ${counter}" >"${plain}"
   npx "$1" --log-level DEBUG \
     --kasEndpoint http://localhost:65432/api/kas \
     --oidcEndpoint http://localhost:65432/auth/realms/tdf \
     --auth tdf-client:123-456 \
     --output sample.txt.tdf \
-    encrypt sample.txt \
+    encrypt "${plain}" \
     --containerType tdf3 \
     --attributes https://example.com/attr/Classification/value/S,https://example.com/attr/COI/value/PRX
 
@@ -58,10 +60,10 @@ _tdf3_test() {
     --containerType tdf3 \
     decrypt sample.txt.tdf
 
-  [ -f sample_out.txt ] && diff sample.txt sample_out.txt
+  [ -f sample_out.txt ] && diff "${plain}" sample_out.txt
 
   echo "Roundtrip tdf3 $1 -> $2 successful!"
-  rm -f sample.txt sample.txt.tdf sample_out.txt
+  rm -f "${plain}" sample.txt.tdf sample_out.txt
 }
 
 _tdf3_test @opentdf/cli-commonjs @opentdf/cli
