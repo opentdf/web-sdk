86f0d48d284fdff6a95b03d03c306bab66ceb0b5
David Mihalcik <dmihalcik@virtru.com>
2022-11-10 08:53:18 -0500

    isolatedModules

--- lib/src/tdf/index.ts
+++ lib/src/tdf/index.ts
@@ -1,7 +1,7 @@
-export { default as AttributeObject, createAttribute } from './AttributeObject';
-export { default as EntityObject } from './EntityObject';
-export { default as PolicyObject } from './PolicyObject';
-export { default as TypedArray } from './TypedArray';
+export { type default as AttributeObject, createAttribute } from './AttributeObject';
+export { type default as EntityObject } from './EntityObject';
+export { type default as PolicyObject } from './PolicyObject';
+export { type default as TypedArray } from './TypedArray';
 export * as EntityObjectHelpers from './EntityObject';
 export { default as Policy } from './Policy';
 export * as Crypto from './Crypto';
