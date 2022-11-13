83196a5008c444cccffdde535c9ee8277ae10c59
Dave Mihalcik <38867245+dmihalcik-virtru@users.noreply.github.com>
2022-11-13 06:30:12 -0500

    🐛 Fix mixing FileClient 'source' and params (#107)
    Previously, the source and params options were exclusive. With this fix, they are properly merged (again)

--- lib/tdf3/src/FileClient.ts
+++ lib/tdf3/src/FileClient.ts
@@ -19,57 +19,80 @@ interface FileClientConfig {
   kasEndpoint?: string;
 }
 
+function isClientTdf3(c: unknown): c is ClientTdf3 {
+  if (typeof c !== 'object') {
+    return false;
+  }
+  return !!((c as ClientTdf3).encrypt && (c as ClientTdf3).decrypt);
+}
+
 export class FileClient {
   dissems: string[] = [];
   dataAttributes: string[] = [];
   private client: ClientTdf3;
 
-  constructor({
-    clientId,
-    clientSecret,
-    oidcRefreshToken,
-    oidcOrigin,
-    kasEndpoint,
-    authProvider,
-  }: FileClientConfig) {
-    this.client = new ClientTdf3({
-      authProvider,
-      clientId,
-      clientSecret,
-      oidcRefreshToken,
-      kasEndpoint,
-      oidcOrigin,
-    });
+  constructor(opts: FileClientConfig | ClientTdf3) {
+    // I'm using a duck type check here to make it easier to mock.
+    if (isClientTdf3(opts)) {
+      this.client = opts;
+    } else {
+      this.client = new ClientTdf3(opts);
+    }
   }
 
-  private static setSource(
-    source: InputSource,
-    params: DecryptParamsBuilder
-  ): Promise<DecryptParams>;
-  private static setSource(
-    source: InputSource,
-    params: EncryptParamsBuilder
-  ): Promise<EncryptParams>;
+  private static setSource(source: InputSource, params: DecryptParamsBuilder): Promise<void>;
+  private static setSource(source: InputSource, params: EncryptParamsBuilder): Promise<void>;
   private static async setSource(
     source: InputSource,
     params: DecryptParamsBuilder | EncryptParamsBuilder
-  ): Promise<EncryptParams | DecryptParams> {
-    if (Buffer && Buffer.isBuffer(source)) {
-      params.setBufferSource(source);
-    }
-    if (source instanceof ArrayBuffer) {
-      params.setArrayBufferSource(source);
-    }
+  ): Promise<void> {
     if (source instanceof Promise) {
       source = await source;
     }
-    if (typeof source === 'string' && params instanceof EncryptParamsBuilder) {
-      source = Readable.toWeb(createReadStream(source));
+    if (Buffer && Buffer.isBuffer(source)) {
+      params.setBufferSource(source);
+      return;
     }
-    if (source instanceof ReadableStream) {
-      params.setStreamSource(source);
+    if (source instanceof ArrayBuffer) {
+      params.setArrayBufferSource(source);
+      return;
     }
-    return params.build();
+    if (typeof source === 'string') {
+      let url;
+      try {
+        url = new URL(source);
+        if (!['http', 'https'].includes(url.protocol)) {
+          url = undefined;
+        }
+      } catch (_) {
+        // Not a url
+      }
+      if (params instanceof EncryptParamsBuilder) {
+        if (url) {
+          const response = await fetch(url);
+          if (!response.ok) {
+            console.warn(
+              `${response.status} Error while fetching [${url}]: [${response.statusText}]`
+            );
+            throw new Error(response.statusText);
+          }
+          if (!response.body) {
+            throw new Error(`${response.status}: No body returned.`);
+          }
+          source = response.body;
+        } else {
+          source = Readable.toWeb(createReadStream(source));
+        }
+        params.setStreamSource(source);
+      } else {
+        // params instanceof DecryptParamsBuilder
+        if (url) {
+          params.setUrlSource(source);
+        } else {
+          params.setFileSource(source);
+        }
+      }
+    }
   }
 
   async encrypt(
@@ -77,31 +100,32 @@ export class FileClient {
     users?: string[],
     params?: EncryptParams
   ): Promise<AnyTdfStream> {
-    const encryptParams = new EncryptParamsBuilder()
-      .withOffline()
-      .withUsersWithAccess(users || this.dissems)
-      .withAttributes(
-        this.dataAttributes.map((attribute) => {
-          return { attribute };
-        })
-      );
-
-    if (params) {
-      return await this.client.encrypt(params);
+    const defaultParams =
+      params ||
+      new EncryptParamsBuilder()
+        .withOffline()
+        .withUsersWithAccess(this.dissems)
+        .withAttributes(
+          this.dataAttributes.map((attribute) => {
+            return { attribute };
+          })
+        )
+        .build();
+    if (users) {
+      // XXX Should this append to existing scope or replace it?
+      defaultParams.scope.dissem = [...users];
     }
-
-    const result = await FileClient.setSource(source, encryptParams);
-    return await this.client.encrypt(result);
+    const paramsBuilder = new EncryptParamsBuilder(defaultParams);
+    await FileClient.setSource(source, paramsBuilder);
+    return await this.client.encrypt(paramsBuilder.build());
   }
 
   async decrypt(source: InputSource = '', params?: DecryptParams): Promise<AnyTdfStream> {
-    const decryptParams = new DecryptParamsBuilder();
-
-    if (params) {
-      return await this.client.decrypt(params);
+    const decryptParams = new DecryptParamsBuilder(params);
+    if (source) {
+      await FileClient.setSource(source, decryptParams);
     }
-    const result = await FileClient.setSource(source, decryptParams);
-    return await this.client.decrypt(result);
+    return await this.client.decrypt(decryptParams.build());
   }
 
   /**
