c5edcc90c27970aed23ab5be611fe09859c7a523
sivanov <sivanov.ctr@virtru.com>
2023-01-16 18:11:43 +0200

    Locker complience fixed

--- lib/src/index.node.ts
+++ lib/src/index.node.ts
@@ -8,9 +8,11 @@ import {
   clientType,
 } from './index.js';
 
-globalThis.crypto ??= crypto as unknown as Crypto;
+if (globalThis) {
+  globalThis.crypto ??= crypto as unknown as Crypto;
 // @ts-expect-error transform type incompatible
-globalThis.ReadableStream ??= ReadableStream;
+  globalThis.ReadableStream ??= ReadableStream;
+}
 
 export { NanoTDFClient, NanoTDFDatasetClient, AuthProviders, version, clientType };
 // for cjs build a default export is needed
