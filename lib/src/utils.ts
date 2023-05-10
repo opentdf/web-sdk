b1bd7b0388f66afd43eaf007e5cdc2c50171966b
David Mihalcik <dmihalcik@virtru.com>
2023-05-10 11:20:38 -0400

    ⬆️ Updates deps
    - Axios needed an update to work in web/test-runner, so just update everything
    - Lets skew estimation work with existing response objects
    - Testing and cleanup

--- lib/src/utils.ts
+++ lib/src/utils.ts
@@ -1,3 +1,5 @@
+import { type AxiosResponseHeaders, type RawAxiosResponseHeaders } from 'axios';
+
 export function isBrowser() {
   return typeof window !== 'undefined'; // eslint-disable-line
 }
@@ -11,14 +13,39 @@ export const rstrip = (str: string, suffix = ' '): string => {
   return str;
 };
 
-export const calculateSKU = async (serverEndpoint = window.location.host): Promise<number> => {
-  const localUnixTimeBefore = Date.now() / 1000;
-  const response = await fetch(serverEndpoint, {
-    method: 'GET',
-    headers: { 'Access-Control-Expose-Headers': 'Date' },
-  });
+/**
+ * Rough estimate of number of seconds to add to the current system clock time
+ * to get the clock time on the given server, or origin if not specified
+ * @param server a server to compute skew with
+ * @returns the number of seconds to add to the current local system clock time
+ * to get an rough guess of the time on the given server
+ */
+export const estimateSkew = async (serverEndpoint = window.origin): Promise<number> => {
+  const localUnixTimeBefore = Date.now();
+  const response = await fetch(serverEndpoint);
+  return estimateSkewFromHeaders(response.headers, localUnixTimeBefore);
+};
 
-  const serverDateString = response.headers.get('Date');
+export type AnyHeaders = AxiosResponseHeaders | RawAxiosResponseHeaders | Headers;
+
+/**
+ * Rough estimate of number of seconds to add to the curren time to get
+ * the clock time on the server that responded with the headers object.
+ * @param headers A set of headers, which must include the `date` header
+ * @param dateNowBefore time before initiating the request, usually by calling
+ * `Date.now()`. Note this is in milliseconds since the epoch, while the
+ * estimate is given in seconds.
+ * @returns the number of seconds to add to the current local system clock time
+ * to get an rough guess of the time on the server that was used
+ */
+export const estimateSkewFromHeaders = (headers: AnyHeaders, dateNowBefore?: number): number => {
+  const localUnixTimeBefore = (dateNowBefore || Date.now()) / 1000;
+  let serverDateString;
+  if (headers.get) {
+    serverDateString = (headers as Headers).get('Date');
+  } else {
+    serverDateString = (headers as AxiosResponseHeaders | RawAxiosResponseHeaders).date;
+  }
   if (serverDateString === null) {
     throw Error('Cannot get access to Date header!');
   }
@@ -27,5 +54,5 @@ export const calculateSKU = async (serverEndpoint = window.location.host): Promi
   const deltaBefore = serverUnixTime - localUnixTimeBefore;
   const deltaAfter = serverUnixTime - localUnixTimeAfter;
 
-  return (deltaBefore + deltaAfter) / 2;
+  return Math.round((deltaBefore + deltaAfter) / 2);
 };
