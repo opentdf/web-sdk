bbca4f766e234c3acc9d8ec18a0d1b00029ff9ff
obidenko <70955025+obidenko@users.noreply.github.com>
2023-05-11 18:40:28 +0300

    ✨ PLAT-2200 : adds clock skew detector (#153)
    Signed-off-by: obidenko <obidenko@virtru.com>
    
    - Estimates clock skew using `Date` header. This header may need to be exposed with [Access-Control-Expose-Headers](https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Access-Control-Expose-Headers) in the response, since `Date` is not in [the CORS safelist](https://developer.mozilla.org/en-US/docs/Glossary/CORS-safelisted_response_header).
    - Axios needed an update to work in web/test-runner, so just update everything
    - Lets skew estimation work with existing response objects
    - This isn't currently used automatically. We will follow up with a use case

--- lib/src/utils.ts
+++ lib/src/utils.ts
@@ -1,3 +1,5 @@
+import { type AxiosResponseHeaders, type RawAxiosResponseHeaders } from 'axios';
+
 export function isBrowser() {
   return typeof window !== 'undefined'; // eslint-disable-line
 }
@@ -9,4 +11,48 @@ export const rstrip = (str: string, suffix = ' '): string => {
     str = str.slice(0, -suffix.length);
   }
   return str;
+};
+
+/**
+ * Rough estimate of number of seconds to add to the current system clock time
+ * to get the clock time on the given server, or origin if not specified
+ * @param server a server to compute skew with
+ * @returns the number of seconds to add to the current local system clock time
+ * to get an rough guess of the time on the given server
+ */
+export const estimateSkew = async (serverEndpoint = window.origin): Promise<number> => {
+  const localUnixTimeBefore = Date.now();
+  const response = await fetch(serverEndpoint);
+  return estimateSkewFromHeaders(response.headers, localUnixTimeBefore);
+};
+
+export type AnyHeaders = AxiosResponseHeaders | RawAxiosResponseHeaders | Headers;
+
+/**
+ * Rough estimate of number of seconds to add to the curren time to get
+ * the clock time on the server that responded with the headers object.
+ * @param headers A set of headers, which must include the `date` header
+ * @param dateNowBefore time before initiating the request, usually by calling
+ * `Date.now()`. Note this is in milliseconds since the epoch, while the
+ * estimate is given in seconds.
+ * @returns the number of seconds to add to the current local system clock time
+ * to get an rough guess of the time on the server that was used
+ */
+export const estimateSkewFromHeaders = (headers: AnyHeaders, dateNowBefore?: number): number => {
+  const localUnixTimeBefore = (dateNowBefore || Date.now()) / 1000;
+  let serverDateString;
+  if (headers.get) {
+    serverDateString = (headers as Headers).get('Date');
+  } else {
+    serverDateString = (headers as AxiosResponseHeaders | RawAxiosResponseHeaders).date;
+  }
+  if (serverDateString === null) {
+    throw Error('Cannot get access to Date header!');
+  }
+  const serverUnixTime = Date.parse(serverDateString) / 1000;
+  const localUnixTimeAfter = Date.now() / 1000;
+  const deltaBefore = serverUnixTime - localUnixTimeBefore;
+  const deltaAfter = serverUnixTime - localUnixTimeAfter;
+
+  return Math.round((deltaBefore + deltaAfter) / 2);
 };
