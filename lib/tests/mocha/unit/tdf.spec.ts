4dea473f096744808cda0a73ae9cbce5b5ff2087
David Mihalcik <dmihalcik@virtru.com>
2023-05-26 15:35:18 -0400

    🐛 Lets unwrapHtml take a string
    We use this behavior in protect-and-track, as part of 'download tdf' hack

--- lib/tests/mocha/unit/tdf.spec.ts
+++ lib/tests/mocha/unit/tdf.spec.ts
@@ -29,7 +29,8 @@ describe('TDF', () => {
     const cipherText = Buffer.from([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2]);
     const transferUrl = 'https://local.virtru.com/start?htmlProtocol=1';
     const wrapped = TDF.wrapHtml(cipherText, JSON.stringify({ thisIs: 'metadata' }), transferUrl);
-    const unwrapped = TDF.unwrapHtml(wrapped);
-    expect(unwrapped).to.eql(cipherText);
+    expect(TDF.unwrapHtml(wrapped)).to.eql(cipherText);
+    expect(TDF.unwrapHtml(wrapped.buffer)).to.eql(cipherText);
+    expect(TDF.unwrapHtml(wrapped.toString('utf-8'))).to.eql(cipherText);
   });
 });
