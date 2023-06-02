a7c8ec0ccdd271385261e41a5c9a67db5e1e3d4b
sivanov <sivanov.ctr@virtru.com>
2023-06-02 13:00:26 +0300

    Revert "Bump"
    This reverts commit d0e6d5caf8e693df287a6a9d8ea801c9f7e7b788.

--- lib/src/version.ts
+++ lib/src/version.ts
@@ -1,7 +1,7 @@
 /**
  * Exposes the released version number of the `@opentdf/client` package
  */
-export const version = '0.5.2';
+export const version = '0.5.1';
 
 /**
  * A string name used to label requests as coming from this library client.
