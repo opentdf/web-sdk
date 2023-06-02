9f25d61d728b39c5f343527d45e63169b2d84121
sivanov <sivanov.ctr@virtru.com>
2023-06-02 13:15:30 +0300

    assume that was the reason

--- .github/workflows/publish-to.sh
+++ .github/workflows/publish-to.sh
@@ -24,8 +24,4 @@ cd ../cli
 npm --no-git-tag-version --allow-same-version version "$v" --tag "$t"
 npm uninstall "@opentdf/client"
 npm install "@opentdf/client@$v"
-npm publish --access public
-
-if [[ "$GITHUB_STEP_SUMMARY" ]]; then
-  echo "### Published ${v}" >>"$GITHUB_STEP_SUMMARY"
 fi
