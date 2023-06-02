3a6bfdea06902a2f822ac6c52205e9867c2c7a28
sivanov <sivanov.ctr@virtru.com>
2023-06-02 16:04:26 +0300

    Bump

--- lib/tdf3/src/version.ts
+++ lib/tdf3/src/version.ts
@@ -1,4 +1,4 @@
 import { inBrowser } from './utils/index.js';
 
-export const version = '0.5.0';
+export const version = '0.5.2';
 export const clientType = inBrowser() ? 'tdf3-js-client' : 'tdf3-js-node';
