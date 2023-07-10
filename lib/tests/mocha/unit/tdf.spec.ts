d71d8defea46665f6bd89b1118022eda35ef3164
sivanov <sivanov.ctr@virtru.com>
2023-07-10 13:47:20 +0300

    Coverage fixed

--- lib/tests/mocha/unit/tdf.spec.ts
+++ lib/tests/mocha/unit/tdf.spec.ts
@@ -1,6 +1,42 @@
 import { expect } from 'chai';
 
 import { TDF } from '../../../tdf3/src/index.js';
+import { AesGcmCipher } from '../../../tdf3/src/ciphers/aes-gcm-cipher.js';
+const sampleCert = `
+-----BEGIN CERTIFICATE-----
+MIIFnjCCA4YCCQCnKw0cfbMLJTANBgkqhkiG9w0BAQsFADCBkDELMAkGA1UEBhMC
+VUExCzAJBgNVBAgMAk9EMQ4wDAYDVQQHDAVPREVTQTEPMA0GA1UECgwGVklSVFJV
+MREwDwYDVQQLDAhQTEFURk9STTEZMBcGA1UEAwwQbG9jYWwudmlydHJ1LmNvbTEl
+MCMGCSqGSIb3DQEJARYWc2l2YW5vdi5jdHJAdmlydHJ1LmNvbTAeFw0yMzA3MDYx
+MTUxMzFaFw0yNDA3MDUxMTUxMzFaMIGQMQswCQYDVQQGEwJVQTELMAkGA1UECAwC
+T0QxDjAMBgNVBAcMBU9ERVNBMQ8wDQYDVQQKDAZWSVJUUlUxETAPBgNVBAsMCFBM
+QVRGT1JNMRkwFwYDVQQDDBBsb2NhbC52aXJ0cnUuY29tMSUwIwYJKoZIhvcNAQkB
+FhZzaXZhbm92LmN0ckB2aXJ0cnUuY29tMIICIjANBgkqhkiG9w0BAQEFAAOCAg8A
+MIICCgKCAgEAn+CvhpQZ6lPZG8j07l8JWWQnmPATKB2GDbcyC/Yg1rJAcFLZmBvH
+VR3SamYQrSKxFRsoBGVruIFQBpoYMSolRWVVRnRBmpB5O8vCdP8J78mRaXGoJAez
+xexuZBEoUIJJAn0Uoxg462201RgzLrZq+b+gOHZzcOfMlh/HPWhTeLdW2i0oMddL
+yeykUa8ARHeCbA1Ux/IZju9n+lY3Pv7pI7z0mYuU14p33lvQbTZ1psY4frwFeyqC
+rqHR1NqKjX+CnitFDrVKV48ZNlBQ6y348NpCaNH0wmy4FRAuGS6dJb9KFK6C+KIj
+EQlWaucPTgvf6/dgZ6IZdLPvHUQNwC9yidRpnyLTUZlxwCXVO3P1sP7ifq75RwFk
+OhvGqnoWNxUwd+23YUOFhE5b7WSO8NwRiMPBeEhNiWaiCmu1v88h52mgRr74MjCQ
+mftuJlPwhsqWMpdYuv42851Y95x5s7BFWtntBNcU/+TTSKcygPNGkuR/tNw79JrM
+bMl/Wj+cJoCW/3+1KzmPX13Nf2Cq2KZ8Kx6JCy+zFj/Hu8O/iqZ1vxA7gUyESPrd
+NFB988QD7H8AQLCeDh6mOTdK08ZG74FlRBSk788A4oyslx8DslOkWbqHLdA7frv6
+rEtl3mJ9UchFVpJOXw5nT3plE0ZRx4NWRLAG2+8GIUsL6eylpFUfVH8CAwEAATAN
+BgkqhkiG9w0BAQsFAAOCAgEAlS3ph3ZcPf90uo0Y0l6Pca5I7ztuq6wu6reKAqnz
+kh6N1rpLSe6EdRIOOCkMWMYTlZYFnSG/USGRfiCV0TOoo2C+jFvARRn6oir1v3B4
+XBQORRNrOP8xV6kXAmNxs9Zl8CpAxfejV0vBHzN709q2HDfwmMmmp/yhlsAa1/iL
+ARYV3TIJBmAOpec2CemuSVwGvYF7Ojl4sTYl0qMzrmm85wMBHJWR9EJbbFZBDDi4
+GKVx26SfxaeauZXRgbO7Tav0q7mbJXI6u95aWO6iwkABRfnEQbimfVCjRI4TKlNs
+WX5PS3QXXJg/9ocEglNGDwTouDwD/yMevR2tJ4clb9SJkJjJ3I0t7aUoFHXBl0B2
+merKiN1ZE4OlbG/BsJypwAqV7Xk6tK3LM8fCMxhP1K0XX5Ifi1+hbN4Wr7a/zpEq
+RWFMXtPKqTne0Ut6M7Xv5BBjY3uBmp0AX9hXt00olAiZsrOfj4gjAVULxdsbaEpr
+nTqI65s26fls4y9bCZSfY//YNMAAtSRfmGbCZfrnzyvMtZfSMHqxhzMX7jXonV9c
+TuHue7faEGaGlWGQVdNeudZaQ/eimDWeWnbhoUz6hSX7NICUFE64W6+GVFsAte3s
+jtfbjUcLqvZzZt+u7YDapWbEbdgYjn/lUt0sTNyY65IagXJel9iacjCIVdzoKPzn
+HJg=
+-----END CERTIFICATE-----
+`.trim();
 
 describe('TDF', () => {
   it('constructs', () => {
@@ -32,5 +68,37 @@ describe('TDF', () => {
     expect(TDF.unwrapHtml(wrapped)).to.eql(cipherText);
     expect(TDF.unwrapHtml(wrapped.buffer)).to.eql(cipherText);
     expect(TDF.unwrapHtml(wrapped.toString('utf-8'))).to.eql(cipherText);
+  });
+
+  it('should fail on invalid cypher param', () => {
+    try {
+      TDF.createCipher('nonexistent cypher')
+    } catch (e) {
+      expect(e.message).to.include('nonexistent cypher');
+    }
+  });
+
+  it('should return cypher', () => {
+    const cypher = TDF.createCipher('aes-256-gcm');
+    expect(cypher instanceof AesGcmCipher).to.equal(true);
+  });
+
+  it('should return key', async () => {
+    const pem = await TDF.extractPemFromKeyString(sampleCert);
+    expect(pem).to.include('-----BEGIN PUBLIC KEY-----');
+    expect(pem).to.include('-----END PUBLIC KEY-----');
+  });
+
+  it('should return pem', async () => {
+    const sampleKey = sampleCert
+      .replace('BEGIN CERTIFICATE', 'BEGIN PUBLIC KEY')
+      .replace('END CERTIFICATE', 'END PUBLIC KEY');
+    const pem = await TDF.extractPemFromKeyString(sampleKey);
+    expect(pem).to.equal(sampleKey);
+  });
+
+  it('should ensure that policy id is uuid format', async () => {
+    const uuid = await TDF.generatePolicyUuid();
+    expect(uuid).to.match(/^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i);
   });
 });
