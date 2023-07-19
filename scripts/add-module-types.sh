76ebc55c8e1b0ce52e6f914095d5d323ff00f94e
Dave Mihalcik <38867245+dmihalcik-virtru@users.noreply.github.com>
2023-07-19 13:19:11 -0400

    🐛 Second attempt at adding back CommonJS Support (#217)
    * Adds package.json type files to lib with the `add-module-types.sh` script
      - These (should) allow tools to correctly identify the module/commonjs type of the relevant .js files in dist

---
+++ scripts/add-module-types.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+# Adds module type definitions to `dist` folders.
+# Allows proper detection of CJS types in older versions of node.
+# Explanation: https://www.sensedeep.com/blog/posts/2021/how-to-create-single-source-npm-module.html
+
+fixup() {
+  local directory_name="$1"
+  shift
+
+  local module_type="$1"
+  shift
+
+  cat >"dist/${directory_name}/package.json" <<EOF
+{
+    "type": "$module_type"
+}
+EOF
+}
+
+if [[ $# -gt 0 ]]; then
+  while [[ $# -gt 0 ]]; do
+    m="$1"
+    shift
+    case "$m" in
+      cjs|commonjs)
+        fixup "$m" commonjs
+        ;;
+      es* | node*)
+        fixup "$m" module
+        ;;
+      *)
+        echo "Unrecognized module type"
+        ;;
+    esac
+  done
+else
+  fixup cjs commonjs
+  fixup web module
+fi
