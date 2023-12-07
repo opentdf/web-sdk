b6caf22f19629886e7d5df709f8b68924f3621e9
Nick Trevino <ntrevino@virtru.com>
2023-12-07 08:52:46 -0700

    Do not await

--- lib/tdf3/src/tdf.ts
+++ lib/tdf3/src/tdf.ts
@@ -947,7 +947,7 @@ async function updateChunkQueue(
             bufferSize
           );
           if (buffer) {
-            await sliceAndDecrypt({
+            sliceAndDecrypt({
               buffer,
               cryptoService,
               reconstructedKeyBinary,
