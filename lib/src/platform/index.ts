7583683c47732d086fceac116442cf06dda73c8f
Patrick Bacon-Blaber <pbacon-blaber@virtru.com>
2024-04-30 14:59:17 -0400

    remove platform

--- lib/src/platform/index.ts
+++
@@ -1,6 +0,0 @@
-import { WellKnownService } from '../../proto/wellknownconfiguration/wellknown_configuration.pb.ts';
-
-// List all static methods of WellKnownService
-Object.keys(WellKnownService).forEach((key) => {
-  console.log(key);
-});
