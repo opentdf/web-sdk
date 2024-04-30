b88ad4714a359cf03001df3195b2fd9ab505253c
Patrick Bacon-Blaber <pbacon-blaber@virtru.com>
2024-04-30 13:56:08 -0400

    remove abc.ts

--- lib/abc.ts
+++
@@ -1,5 +0,0 @@
-import { fetchReq } from './proto/fetch.pb';
-
-import { AuthorizationService } from './proto/authorization/authorization.pb';
-
-AuthorizationService.GetEntitlements({ entities: ['']})
