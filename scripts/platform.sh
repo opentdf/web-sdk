d4693e4deab60bdc7faf9dc8bfedb6a4ee310dce
Krish Suchak <suchak.krish@gmail.com>
2024-05-09 15:05:43 -0400

    clean up

--- scripts/platform.sh
+++ scripts/platform.sh
@@ -1,12 +1,13 @@
-#!/usr/bin/env zsh
+#!/usr/bin/env bash
 
 # Fetch latest platform code
-# rm -rf platform
-# git clone https://github.com/opentdf/platform.git
-
-# Copy platform protos to proto directory (NOT NECESSARY)
-# mkdir -p proto
-# cp platform/**/*.proto proto
+rm -rf platform
+git clone https://github.com/opentdf/platform.git
 
 # Generate Typescript code
+cmd=$(npx buf --version)
+if [ $? -ne 0 ]; then
+  npm install @bufbuild/protobuf @bufbuild/protoc-gen-es @bufbuild/buf
+fi
 npx buf generate platform/service
+echo "Generated Typescript code from Protobuf files (src: platform/service, dst: lib/src/platform)" 
