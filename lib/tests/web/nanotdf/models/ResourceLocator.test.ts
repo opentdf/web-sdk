f4f31f23e387119ea3644c697e63d9948627e2f6
David Mihalcik <dmihalcik@virtru.com>
2024-08-28 09:43:42 -0400

    Update ResourceLocator.test.ts

--- lib/tests/web/nanotdf/models/ResourceLocator.test.ts
+++ lib/tests/web/nanotdf/models/ResourceLocator.test.ts
@@ -5,50 +5,56 @@ import ResourceLocatorIdentifierEnum from '../../../../src/nanotdf/enum/Resource
 import { hex } from '../../../../src/encodings/index.js';
 
 describe('NanoTDF.ResourceLocator', () => {
-  for (const { u, kid, idt } of [
-    { u: 'http://a', idt: ResourceLocatorIdentifierEnum.None },
-    { u: 'http://a', kid: 'r1', idt: ResourceLocatorIdentifierEnum.TwoBytes },
-    { u: 'http://a', kid: '12345678', idt: ResourceLocatorIdentifierEnum.EightBytes },
+  for (const { u, kid, idt, v } of [
+    { u: 'http://a', idt: ResourceLocatorIdentifierEnum.None, v: '00 01 61' },
+    { u: 'https://a', idt: ResourceLocatorIdentifierEnum.None, v: '01 01 61' },
+    { u: 'http://a', kid: 'a', idt: ResourceLocatorIdentifierEnum.TwoBytes, v: '10 01 61 61 00' },
+    { u: 'http://a', kid: 'r1', idt: ResourceLocatorIdentifierEnum.TwoBytes, v: '10 01 61 72 31' },
+    { u: 'https://a', kid: 'a', idt: ResourceLocatorIdentifierEnum.TwoBytes, v: '11 01 61 61 00' },
+    { u: 'https://a', kid: 'r1', idt: ResourceLocatorIdentifierEnum.TwoBytes, v: '11 01 61 72 31' },
     {
       u: 'http://a',
-      kid: '12345678901234567890123456789012',
-      idt: ResourceLocatorIdentifierEnum.ThirtyTwoBytes,
+      kid: '12345678',
+      idt: ResourceLocatorIdentifierEnum.EightBytes,
+      v: '20 01 61 31 32 33 34 35 36 37 38',
     },
-  ]) {
-    it(`ResourceLocator.fromURL("${u}", "${kid}")`, () => {
-      const rl = ResourceLocator.fromURL(u, kid);
-      expect(rl).to.have.property('identifierType', idt);
-      expect(rl).to.have.property('identifier', kid);
-    });
-  }
-
-  for (const { u, kid, v } of [
-    { u: 'http://a', v: '00 01 61' },
-    { u: 'https://a', v: '01 01 61' },
-    { u: 'http://a', kid: 'a', v: '10 01 61 61 00' },
-    { u: 'http://a', kid: 'r1', v: '10 01 61 72 31' },
-    { u: 'https://a', kid: 'a', v: '11 01 61 61 00' },
-    { u: 'https://a', kid: 'r1', v: '11 01 61 72 31' },
-    { u: 'http://a', kid: '12345678', v: '20 01 61 31 32 33 34 35 36 37 38' },
-    { u: 'http://a', kid: '12345', v: '20 01 61 31 32 33 34 35 00 00 00' },
-    { u: 'http://a', kid: '12345678', v: '20 01 61 31 32 33 34 35 36 37 38' },
+    {
+      u: 'http://a',
+      kid: '12345',
+      idt: ResourceLocatorIdentifierEnum.EightBytes,
+      v: '20 01 61 31 32 33 34 35 00 00 00',
+    },
+    {
+      u: 'http://a',
+      kid: '12345678',
+      idt: ResourceLocatorIdentifierEnum.EightBytes,
+      v: '20 01 61 31 32 33 34 35 36 37 38',
+    },
     {
       u: 'http://a',
       kid: '1234567890123456',
+      idt: ResourceLocatorIdentifierEnum.ThirtyTwoBytes,
       v: '30 01 61 31 32 33 34 35 36 37 38 39 30 31 32 33 34 35 36 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00',
     },
     {
       u: 'http://a',
       kid: '12345678901234567890123456789012',
+      idt: ResourceLocatorIdentifierEnum.ThirtyTwoBytes,
       v: '30 01 61 31 32 33 34 35 36 37 38 39 30 31 32 33 34 35 36 37 38 39 30 31 32 33 34 35 36 37 38 39 30 31 32',
     },
   ]) {
+    const hexValue = v.replace(/\s/g, '');
+    const ab = hex.decodeArrayBuffer(hexValue);
     it(`ResourceLocator.parse() => (${u}, "${kid}")`, () => {
-      const hexValue = v.replace(/\s/g, '');
-      const ab = hex.decodeArrayBuffer(hexValue);
       const rl = ResourceLocator.parse(new Uint8Array(ab));
       expect(rl).to.have.property('identifier', kid);
       expect(rl).to.have.property('url', u);
+    });
+    it(`ResourceLocator.fromURL("${u}", "${kid}")`, () => {
+      const rl = ResourceLocator.fromURL(u, kid);
+      expect(rl).to.have.property('identifierType', idt);
+      expect(rl).to.have.property('identifier', kid);
+      expect(hex.encodeArrayBuffer(rl.toBuffer())).to.eql(hexValue);
     });
   }
 
