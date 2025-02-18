82a70a030b82ea0a061398c824b674bb21e9b60b
David Mihalcik <dmihalcik@virtru.com>
2025-02-18 12:42:49 -0500

    Update eslint.config.mjs

--- web-app/eslint.config.mjs
+++ web-app/eslint.config.mjs
@@ -1,8 +1,10 @@
 import globals from "globals";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
 import js from "@eslint/js";
 import { FlatCompat } from "@eslint/eslintrc";
+import pluginChaiFriendly from "eslint-plugin-chai-friendly";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -12,7 +14,11 @@ const compat = new FlatCompat({
     allConfig: js.configs.all
 });
 
-export default [...compat.extends("plugin:@typescript-eslint/recommended", "prettier"), {
+export default [...compat.extends("plugin:@typescript-eslint/recommended", "plugin:prettier/recommended"), {
+    plugins: {
+        "chai-friendly": pluginChaiFriendly,
+        "@typescript-eslint": typescriptEslint,
+    },
     languageOptions: {
         globals: {
             ...globals.browser,
@@ -24,5 +30,11 @@ export default [...compat.extends("plugin:@typescript-eslint/recommended", "pret
         parserOptions: {
             project: ["**/tsconfig.json"],
         },
+
+    },
+    rules: {
+        // See https://www.npmjs.com/package/eslint-plugin-chai-friendly
+        "@typescript-eslint/no-unused-expressions": "off", // disable original rule
+        "chai-friendly/no-unused-expressions": "error"
     },
 }];
