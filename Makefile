4885670fa62df5620e4a9b27a764b0f155b80a32
David Mihalcik <dmihalcik@virtru.com>
2025-02-27 14:42:39 -0500

    feature(lib): Bump for EC feature

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 
-version=0.2.0
+version=0.3.0
 extras=cli web-app
 pkgs=lib $(extras)
 
--- lib/src/version.ts
+++ lib/src/version.ts
@@ -1,7 +1,7 @@
 /**
  * Exposes the released version number of the `@opentdf/sdk` package
  */
-export const version = '0.2.0';
+export const version = '0.3.0';
 
 /**
  * A string name used to label requests as coming from this library client.
