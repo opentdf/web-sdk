66dd54352f019571f3a1f221a7009fe13749f3a8
David Mihalcik <dmihalcik@virtru.com>
2025-03-26 09:07:30 -0400

    feat(sdk): Target container parameter
    This allows interoperability with older clients

--- lib/src/version.ts
+++ lib/src/version.ts
@@ -11,4 +11,16 @@ export const clientType = 'web-sdk';
 /**
  * Version of the opentdf/spec this library is targeting
  */
-export const tdfSpecVersion = '4.3.0';
+export const tdfSpecVersion: SupportedTDFSpecVersion = '4.3.0';
+
+export type SupportedTDFSpecVersion = '4.2.2' | '4.3.0';
+
+export type KnownTDFSpecVersion = SupportedTDFSpecVersion;
+
+export function isSupportedTDFSpecVersion(version: string): version is SupportedTDFSpecVersion {
+  return version === '4.2.2' || version === '4.3.0';
+}
+
+export function isKnownTDFSpecVersion(version: string): version is KnownTDFSpecVersion {
+  return isSupportedTDFSpecVersion(version);
+}
