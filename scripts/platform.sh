ef4dda4c75ef62edab9cbf8d70ecb566fb78c4ef
Eugene Yakhnenko <eugene.yakhnenko@virtru.com>
2025-04-15 15:29:55 -0700

    feat(sdk): connect rpc client export

--- scripts/platform.sh
+++ scripts/platform.sh
@@ -7,9 +7,6 @@ rm -rf platform lib/src/platform
 git clone https://github.com/opentdf/platform.git
 
 # Generate Typescript code
-cmd=$(npx buf --version)
-if [ $? -ne 0 ]; then
-  npm install @bufbuild/protoc-gen-es @bufbuild/buf @connectrpc/protoc-gen-connect-es
-fi
-npx buf generate platform/service
+cd lib
+npx buf generate ../platform/service
 echo "Generated Typescript code from Protobuf files (src: platform/service, dst: lib/src/platform)"
