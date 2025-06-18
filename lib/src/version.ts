5d7669e54002adb8258d1360097a2996656a3302
Eugene Yakhnenko <eugene.yakhnenko@virtru.com>
2025-06-18 14:01:16 -0700

    🆙 0.3.3 patch autobump

--- lib/src/version.ts
+++ lib/src/version.ts
@@ -1,7 +1,7 @@
 /**
  * Exposes the released version number of the `@opentdf/sdk` package
  */
-export const version = '0.3.2';
+export const version = '0.3.3';
 
 /**
  * A string name used to label requests as coming from this library client.
