945c4dea75e237a86088738a29906cfb1b266fc0
Eugene Yakhnenko <eugene.yakhnenko@virtru.com>
2025-06-20 10:27:49 -0700

    chore: add markdown plugin
    docs: adds and updates some docs comments
    
    chore: adds platform client and updates more docs
    
    Signed-off-by: Eugene Yakhnenko <eugene.yakhnenko@virtru.com>

--- lib/src/index.ts
+++ lib/src/index.ts
@@ -2,6 +2,7 @@ export { type AuthProvider, type HttpMethod, HttpRequest, withHeaders } from './
 export * as AuthProviders from './auth/providers.js';
 export { attributeFQNsAsValues } from './policy/api.js';
 export { version, clientType, tdfSpecVersion } from './version.js';
+export { PlatformClient, type PlatformClientOptions, type PlatformServices } from './platform.js';
 export * from './opentdf.js';
 export * from './seekable.js';
 export * from '../tdf3/src/models/index.js';
