a4039fa1ce6f2220af1b81a77459c03787d5ffe1
Morgan Kleene <mkleene@virtru.com>
2025-07-23 17:55:45 +0200

    format
    Signed-off-by: Morgan Kleene <mkleene@virtru.com>

--- lib/tests/mocha/unit/assertions.spec.ts
+++ lib/tests/mocha/unit/assertions.spec.ts
@@ -24,23 +24,24 @@ describe('assertions', () => {
 
     it('normalizes assertions', async () => {
       let assertion: any = {
-        appliesToState: "unencrypted",
-        id: "system-metadata",
+        appliesToState: 'unencrypted',
+        id: 'system-metadata',
         binding: {
-          method: "jws",
-          signature: "test-signature"
+          method: 'jws',
+          signature: 'test-signature',
         },
         signingKey: {
-          "alg": "ES256",
-          "key": new Uint8Array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]),
+          alg: 'ES256',
+          key: new Uint8Array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]),
         },
-        scope: "payload",
+        scope: 'payload',
         statement: {
-          format: "json",
-          schema: "system-metadata-v1",
-          value: "{\"tdf_spec_version\":\"4.3.0\",\"creation_date\":\"2025-07-23T09:25:51.255364+02:00\",\"operating_system\":\"Mac OS X\",\"sdk_version\":\"Java-0.8.2-SNAPSHOT\",\"java_version\":\"17.0.14\",\"architecture\":\"aarch64\"}"
+          format: 'json',
+          schema: 'system-metadata-v1',
+          value:
+            '{"tdf_spec_version":"4.3.0","creation_date":"2025-07-23T09:25:51.255364+02:00","operating_system":"Mac OS X","sdk_version":"Java-0.8.2-SNAPSHOT","java_version":"17.0.14","architecture":"aarch64"}',
         },
-        type: "other"
+        type: 'other',
       };
 
       let h1 = await assertions.hash(assertion);
