d3cdc202c7fa30c4e7d5972950a9278cd2d3943b
Elizabeth Healy <ehealy@virtru.com>
2025-10-21 12:38:16 -0400

    add a workflow to update the pbs

--- scripts/platform.sh
+++ scripts/platform.sh
@@ -2,14 +2,22 @@
 
 set -eu
 
-# Fetch latest platform code
+
 rm -rf platform lib/src/platform
-git clone https://github.com/opentdf/platform.git
+
+if [ -n "${PLATFORM_SRC:-}" ]; then
+  # Use PLATFORM_SRC for buf generate
+  echo "Using PLATFORM_SRC: $PLATFORM_SRC"
+else
+  # Clone latest platform code
+  git clone https://github.com/opentdf/platform.git
+  PLATFORM_SRC="platform/service"
+fi
 
 # Generate Typescript code
 cd lib
 # version
 PATH="$PWD/node_modules/.bin:$PATH" buf --version
 # Ensure we use the local protoc-gen-es from node_modules
-PATH="$PWD/node_modules/.bin:$PATH" buf generate ../platform/service
-echo "Generated Typescript code from Protobuf files (src: platform/service, dst: lib/src/platform)"
+PATH="$PWD/node_modules/.bin:$PATH" buf generate "$PLATFORM_SRC"
+echo "Generated Typescript code from Protobuf files (src: $PLATFORM_SRC, dst: lib/src/platform)"
