18a75d7ef6f00b2e74505df1eb21e565e11e4c9d
Devin Collins <3997333+ImDevinC@users.noreply.github.com>
2026-01-14 06:29:31 -0800

    chore(ci): SRE-5791 Revert OIDC CI changes (#801)
    * Revert "chore(ci): SRE-5791 Update action and debug npmrc file (#799)"
    
    This reverts commit 11e8f7897d8cd625ab61da068aa81ee5ab0309b3.
    
    * Revert "chore(ci): SRE-5791 Unset token when using provenence (#798)"
    
    This reverts commit 5aed12032a459c02008d996c237d588209f44268.
    
    * Revert "feat(ci): add npm provenance support for package publishing (#797)"
    
    This reverts commit 4d342ce714759a62cd6a51f341adf1bbae72b5c4.
    
    * Revert "chore(ci): SRE-5791 Update workflows to use OIDC (#796)"
    
    This reverts commit 873e95a6b30e45efa8af69f963b0fee4ceb5977d.

--- scripts/deliver-to-npm-registry.sh
+++ scripts/deliver-to-npm-registry.sh
@@ -6,14 +6,6 @@ set -exuo pipefail
 version="${1%%+*}"
 tag="${2}"
 
-# Build provenance flag if NPM_PROVENANCE is set to "true"
-provenance_flag=""
-if [[ "${NPM_PROVENANCE:-}" == "true" ]]; then
-  provenance_flag="--provenance"
-  unset NODE_AUTH_TOKEN # This is needed to force OIDC, as the setup-node action sets NODE_AUTH_TOKEN https://github.com/actions/setup-node/issues/1440
-  cat /home/runner/work/_temp/.npmrc # This is for debugging purposes to see if any auth tokens are set, we should remove it later
-fi
-
 cd lib
 file=src/version.ts
 if ! sed "s|export const version = \'[^']\{1,\}\'; // x-release-please-version\$|export const version = \'${version}\';|" "${file}" >"${file}.tmp"; then
@@ -23,7 +15,7 @@ fi
 mv "${file}.tmp" "${file}"
 
 npm version --no-git-tag-version --allow-same-version "$version"
-npm publish --access public --tag "$tag" $provenance_flag
+npm publish --access public --tag "$tag"
 
 # Wait for npm publish to go through...
 sleep 5
@@ -32,7 +24,7 @@ cd "../cli"
 npm version --no-git-tag-version --allow-same-version "$version"
 npm uninstall "@opentdf/sdk"
 npm install "@opentdf/sdk@$version"
-npm publish --access public --tag "$tag" $provenance_flag
+npm publish --access public --tag "$tag"
 
 if [[ "$GITHUB_STEP_SUMMARY" ]]; then
 	echo "### Published ${version} (${tag})" >>"$GITHUB_STEP_SUMMARY"
